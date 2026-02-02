c60f42b91dcff2fe881fb9c858df2bb429a0d405
samarth-sakri <samarthsakri876@gmail.com>
2026-02-02 21:09:21 +0530

    Update proc_load_silver.sql

--- scripts/Silver/proc_load_silver.sql
+++ scripts/Silver/proc_load_silver.sql
@@ -1,1 +1,239 @@
+/*
+=========================================================================================
+Stored Procedure : Load silver layer (Bronze - Silver)
+=========================================================================================
+Script Purpsse:
+  This stored procedure performs the ETL (Extract Transfer Load) process to populate 
+  the 'Silver' schema from the 'bronze' schema.
+Action Performance :
+ -Truncates silver tables 
+ -Insert Transformed and cleansed data from bronze into silver tables.
 
+Parameters:
+  None 
+  This stored procedures does not accept any parametrs or return any values.
+
+Usage example :
+ call silver.load_silver();
+
+=========================================================================================
+*/
+create or replace procedure silver.load_silver() language plpgsql
+as $$
+Declare 
+    start_time timestamp;
+    end_time timestamp;
+begin 
+    raise notice'================================================';
+	raise notice'loading silver layer';
+	raise notice '===============================================';
+
+	raise notice '===============================================';
+	raise notice 'loading crm tables';
+	raise notice '===============================================';
+
+	--crm_cust_info
+	
+    raise notice 'Truncating table : silver.crm_cust_info';
+    truncate table silver.crm_cust_info;
+	
+	start_time := clock_timestamp();
+    raise notice 'inserting data into: silver.crm_cust_info';
+    insert into silver.crm_cust_info (
+	   cst_id, 
+	   cst_key,
+	   cst_firstname,
+	   cst_lastname,
+	   cst_gndr,
+	   cst_marital_status,
+	   cst_create_date)
+    select 
+       cst_id,
+       cst_key,
+    trim(cst_firstname) as cst_firstname,
+    trim(cst_lastname)as cst_lastname,
+    case 
+       when upper(trim(cst_gndr)) = 'M' then 'Male'
+       when upper(trim(cst_gndr)) = 'F' then 'Female'
+       else 'N/A'
+    end cst_gndr,
+    case 
+       when upper(trim(cst_marital_status)) = 's' then 'Single'
+       when upper(trim(cst_marital_status ))= 'M' then 'Married'
+    else 'N/A'
+    end cst_marital_status,
+        cst_create_date 
+    from 
+      (select *,
+       row_number() over (partition by cst_id  order by cst_create_date desc )as flag_last
+       from bronze.crm_cust_info
+       where cst_id is not null)t
+    where flag_last = 1 ;
+	end_time := clock_timestamp();
+	raise notice 'Total Time Taken%:',end_time -start_time;
+	
+	--crm_prod_info()
+    raise notice 'Truncating table : silver.crm_prd_info';
+    truncate table silver.crm_prd_info;
+
+	start_time := clock_timestamp();
+    raise notice 'inserting data into: silver.crm_cust_info';
+    insert into silver.crm_prd_info(
+        prd_id,
+        cat_id,
+        prd_key,  
+        prd_nm ,  
+        prd_cost, 
+        prd_line ,
+        prd_start_dt, 
+        prd_end_dt
+    )
+    select 
+        prd_id,
+        replace(substring(prd_key,1,5),'-','_')as cat_id,
+        substring(prd_key,7,length(prd_key))as prd_key,
+        prd_nm,
+        coalesce(prd_cost, 0) as prd_cost,
+        case 
+          when upper(trim(prd_line))= 'M'then 'Mountain' 
+		  when upper(trim(prd_line))= 'R'then 'Road'
+		  when upper(trim(prd_line))= 'S'then 'Other Sales'
+		  when upper(trim(prd_line))= 'T'then 'Touring'
+		else 'n/a'
+        end prd_line,
+        cast(prd_start_dt as date ) as prd_start_dt,
+        cast(lead(prd_start_dt) over(partition by prd_key order by prd_start_dt)-iNTERVAL '1 day' as date) as prd_end_dt
+    from bronze.crm_prod_info;
+	end_time := clock_timestamp();
+	raise notice 'Total Time Taken%:',end_time -start_time;
+	
+	--crm_sales_details 
+	
+    raise notice 'Truncating table : silver.crm_sales_details';
+    truncate table silver.crm_sales_details;
+	
+	start_time := clock_timestamp();
+    raise notice 'inserting data into: silver.crm_sales_details';
+    insert into silver.crm_sales_details(
+        sls_ord_num,
+        sls_prd_key, 
+        sls_cust_id, 
+        sls_order_dt,
+        sls_ship_dt,
+        sls_due_dt,
+        sls_sales,
+        sls_quantity,
+        sls_price
+    )
+    select 
+        sls_ord_num,
+        sls_prd_key,
+        sls_cust_id,
+    case 
+        when sls_order_dt = 0 or length(cast(sls_order_dt as text)) != 8 then null 
+	else cast(cast(sls_order_dt as varchar)as date ) 
+    end as sls_order_dt ,
+    case 
+        when sls_ship_dt = 0 or length(cast(sls_ship_dt as text))!= 8 then null 
+    else cast(cast(sls_ship_dt as varchar)as date)
+    end as sls_ship_dt,
+    case 
+      when sls_due_dt = 0 or length(cast(sls_due_dt as text))!= 8 then null 
+    else cast(cast(sls_due_dt as varchar)as date)
+    end as sls_due_dt,
+    case 
+      when sls_sales is null or sls_sales <=0 or sls_sales != sls_quantity * abs(sls_price)
+	  then sls_quantity * abs(sls_price)
+	  else sls_sales 
+      end as sls_sales,
+    sls_quantity,
+    case 
+      when sls_price is null or sls_price <= 0 then sls_sales/nullif(sls_quantity, 0 )
+	  else sls_price 
+      end as sls_price
+    from bronze.crm_sales_details ;	
+	end_time := clock_timestamp();
+	raise notice 'Total Time Taken%:',end_time -start_time;
+	
+	--erp_cust_az12;
+	raise notice '====================================================';
+	raise notice 'loading erp tables';
+	raise notice '====================================================';
+	
+    raise notice 'Truncating table :silver.erp_cust_az12';
+    truncate table silver.erp_cust_az12;
+	
+	start_time := clock_timestamp();
+    raise notice 'inserting data into: silver.erp_cust_az12';
+    insert into silver.erp_cust_az12(
+      cid,
+	  bdate,
+	  gen
+    )
+    select 
+    case 
+       when cid like 'NAS%' then substring (cid, 4,length(cid))
+	   else cid 
+    end as cid ,
+    case 
+       when bdate > now() then null 
+	   else bdate 
+    end ,
+    case 
+       when upper(trim(gen)) in ('F','FEMALE') THEN 'Female'
+	   when upper(trim(gen)) in ('M','MALE') then 'Male '
+	   else 'n/a'
+    end gen 
+    from bronze.erp_cust_az12;
+	end_time := clock_timestamp();
+	raise notice 'Total Time Taken%:',end_time -start_time;
+	
+      --erp_loca101
+	raise notice 'Truncating table :silver.erp_loca101';
+	truncate table silver.erp_loca101;
+	
+	start_time := clock_timestamp(); 
+    raise notice 'inserting data into : silver.erp_loca101';
+    insert into silver.erp_loca101(
+      cid,
+      cntry
+     )
+    select 
+    replace(cid,'-','') cid ,
+    case 
+      when trim(cntry)= 'DE' then 'Germany'
+      when trim(cntry) in ('US','USA') then 'United States'
+      when trim(cntry)='' or cntry is null then 'n/a'
+      else trim(cntry)
+    end as cntry
+    from bronze.erp_loca101 ;
+	end_time := clock_timestamp();
+	raise notice 'Total Time Taken%:',end_time -start_time;
+
+    raise notice 'Truncating table :silver.erp_px_cat_g1v2';
+    truncate table silver.erp_px_cat_g1v2;
+	
+	start_time:= clock_timestamp();
+    raise notice 'inserting data into : silver.erp_px_cat_g1v2';
+    insert into silver.erp_px_cat_g1v2(
+       id,
+       cat,
+       subcat,
+       maintenance
+     )
+    select 
+       id,
+       cat,
+       subcat,
+       maintenance
+    from bronze.erp_px_cat_g1v2;
+	end_time := clock_timestamp();
+	raise notice 'Total Time Taken %:',end_time - start_time;
+  Exception 
+	when  others then 
+	raise notice '=================================================';
+	raise notice 'Error number %',SQLERRM;
+	raise notice 'Erroe number %',SQLSTATE;
+	raise notice '==================================================';
+end ;
+$$;
