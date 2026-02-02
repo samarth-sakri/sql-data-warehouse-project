9dfefdd16445054b2ecbd34dd21a62428957f453
samarth-sakri <samarthsakri876@gmail.com>
2026-02-02 21:43:22 +0530

    Update quality_checks_silver.sql

--- tests/quality_checks_silver.sql
+++ tests/quality_checks_silver.sql
@@ -48,3 +48,43 @@ select distinct prd_line from silver.crm_prod_info;
 select prd_start_dt,prd_end_dt from silver.crm_prod_info
 where prd_end_dt < prd_start_dt;
 
+--========================================================================================
+--Checking silver.crm_sales_details 
+--========================================================================================
+--check the null values and duplicates in a primary key 
+--Exceptation : No Date Invalid 
+select 
+nullif(sls_ship_dt, 0) as sls_ship_dt
+from bronze.crm_sales_details
+where sls_ship_dt <= 0 or length(cast(sls_ship_dt as text))!= 8 or sls_ship_dt > 20500101
+or sls_ship_dt <  19000101;
+
+select sls_order_dt from bronze.crm_sales_details
+where  length(cast(sls_order_dt as text)) != 8
+or sls_order_dt > 20500101
+or sls_order_dt < 19000101;
+
+--check for invalid data orders (Order_date > Ship_date)
+--Expected : No result 
+select * from silver.crm_sales_details 
+where sls_order_dt > sls_ship_dt or sls_order_dt > sls_due_dt;
+
+-- check data consistency: between sales,quantity, and price 
+--Sales = Quantity * price 
+--Values must not be null,zero or negative.
+select distinct 
+sls_sales,
+sls_quantity,
+sls_price
+from silver.crm_sales_details
+where sls_sales != sls_quantity * sls_price or sls_sales is null or sls_quantity is null 
+or sls_price is null or
+sls_sales <=0  or sls_quantity <= 0 
+or sls_price <= 0 
+order by sls_sales, sls_quantity,sls_price;
+
+select sls_ord_num from silver.crm_sales_details 
+where sls_ord_num != trim(sls_ord_num);
+select * from silver.crm_sales_details;
+
+
